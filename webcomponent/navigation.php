6a39ddeaec67e6e478974c395834b8c64c4f0392
Lekhraj KR <mailtoahuman@gmail.com>
2015-09-23 11:32:43 +0530

    Create navigation.php

---
+++ webcomponent/navigation.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid ">
+    <div class="navbar-header form-group">
+      <a class="navbar-brand" href="index.php">Kodayickal Internet Cafe</a> 
+    </div>
+	
+   <form class="navbar-form navbar-left" action="search.php" method="post" autocomplete="off">
+        <div class="form-group">
+          <input type="text" name="mobile" class="form-control" placeholder="Search..." >
+        </div>
+        <button type="submit" class="btn btn-primary" name="submit">Search</button>
+      </form>
+
+  </div>
+</nav>
